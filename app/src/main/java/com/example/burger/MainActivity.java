c8aec84527c24bbdbf71e9220cfacc8e4d7f8e35
kikewest <ebellidoperejil2@gmail.com>
2023-11-09 20:46:52 +0100

    cambios fragment y fondo

--- app/src/main/java/com/example/burger/MainActivity.java
+++ app/src/main/java/com/example/burger/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.burger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,10 +11,18 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentTransaction transaction;
+
+    Fragment fragmentLogin, fragmentInicio, fragmentRegistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentInicio = new inicioFragment();
+        fragmentLogin = new LoginFragment();
+        fragmentRegistro = new registroFragment();
+
 
         Button btncrear = findViewById(R.id.crear);
         Button btninicio = findViewById(R.id.iniciar);
@@ -20,15 +30,19 @@ public class MainActivity extends AppCompatActivity {
 
         btncrear.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, crearcuenta.class);
-                startActivity(intent);
+                transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragmentContainerView, fragmentRegistro);
+                transaction.addToBackStack(null);
+                transaction.commit();
             }
         });
 
         btninicio.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, inicio.class);
-                startActivity(intent);
+                transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragmentContainerView, fragmentLogin);
+                transaction.addToBackStack(null);
+                transaction.commit();
             }
         });
     }
