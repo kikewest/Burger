694cb925b9008fce947ea56fa9af2afa6a5b9998
kikewest <ebellidoperejil2@gmail.com>
2023-11-10 22:11:52 +0100

    JESUS NO VEA
    no veas la que as liao pollito

--- app/src/main/java/com/example/burger/registroFragment.java
+++ app/src/main/java/com/example/burger/registroFragment.java
@@ -4,6 +4,8 @@ import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class registroFragment extends Fragment {
+    FragmentTransaction transaction;
     private DbHelper dbHelper;
     private EditText editTextNombre, editTextCorreo, editTextDireccion, editTextTelefono, editTextContrasenia;
 
@@ -30,7 +33,10 @@ public class registroFragment extends Fragment {
         btniniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragmentContainerView, new LoginFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
             }
         });
         btnCrear.setOnClickListener(new View.OnClickListener() {
