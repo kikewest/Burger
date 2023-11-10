adc0c2a99a29fc665cf855c56376de479a8454a6
kikewest <ebellidoperejil2@gmail.com>
2023-11-10 04:58:18 +0100

    apk V1.0.0

--- app/src/main/java/com/example/burger/MainActivity.java
+++ app/src/main/java/com/example/burger/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.burger;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -58,7 +60,10 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Log
         });
         ConstraintLayout mainLayout = findViewById(R.id.activity_main_layout);
         mainLayout.setBackgroundResource(R.drawable.fondosintrans);
-
+        //el siguiente if cambia la barra de navegacion de android de morado a transparente
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.transparente)); // Reemplaza R.color.colorTransparent con tu color deseado
+        }
     }
     @Override
     public void onLogin(String username, String password) {
