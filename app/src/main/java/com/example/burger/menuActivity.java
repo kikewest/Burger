6ca2e318237ede017fcdcb41b686ddb3fb1d8be6
jesus <jesus.gomez1992@gmail.com>
2023-11-27 20:32:54 +0100

    invitado terminado

--- app/src/main/java/com/example/burger/menuActivity.java
+++ app/src/main/java/com/example/burger/menuActivity.java
@@ -1,8 +1,10 @@
 package com.example.burger;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,11 +27,31 @@ public class menuActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("mostrarFragmento")) {
+            String fragmento = intent.getStringExtra("mostrarFragmento");
 
+            // Determina qué fragmento debe mostrarse según la información del Intent
+            if (fragmento.equals("firstFragment")) {
+                loadFragment(firstFragment);
+            } else if (fragmento.equals("secondFragment")) {
+                loadFragment(secondFragment);
+            } else if (fragmento.equals("thirdFragment")) {
+                loadFragment(thirdFragment);
+            }
+        } else {
+            // Si no hay información en el Intent, carga el FirstFragment por defecto
+            loadFragment(firstFragment);
+        }
 
-        loadFragment(firstFragment);
-
+        // Muestra el BottomNavigationView si no se ha ocultado
+        if (navigation.getVisibility() == View.GONE) {
+            navigation.setVisibility(View.VISIBLE);
+        }
     }
+
+
+
 
     private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -53,5 +75,19 @@ public class menuActivity extends AppCompatActivity {
         transaction.replace(R.id.frame_container, fragment);
         transaction.commit();
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("mostrarFragmento")) {
+            String fragmento = intent.getStringExtra("mostrarFragmento");
+
+            if (fragmento.equals("firstFragment")) {
+                // Si se debe mostrar solo el FirstFragment, oculta el BottomNavigationView
+                BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+                navigationView.setVisibility(View.GONE);
+            }
+        }
+    }
 }
