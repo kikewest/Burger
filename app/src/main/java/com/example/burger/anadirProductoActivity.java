6b45136be943d14311c1cc87b39635068d1bd6aa
kikewest <ebellidoperejil2@gmail.com>
2023-12-10 00:00:55 +0100

    crud terminado y app lista para entrega

--- app/src/main/java/com/example/burger/anadirProductoActivity.java
+++ app/src/main/java/com/example/burger/anadirProductoActivity.java
@@ -7,11 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class anadirProductoActivity extends AppCompatActivity {
 
-    private EditText nombreEditText, precioEditText, categoriaEditText, descripcionEditText, stockEditText;
+    private EditText nombreEditText, precioEditText, categoriaEditText, descripcionEditText, stockEditText, imagenEditText;
     private DbHelper dbHelper;
 
     @Override
@@ -25,6 +26,7 @@ public class anadirProductoActivity extends AppCompatActivity {
         categoriaEditText = findViewById(R.id.Categoria);
         descripcionEditText = findViewById(R.id.Descripcion);
         stockEditText = findViewById(R.id.Stock);
+        imagenEditText = findViewById(R.id.Imagen);
 
         Button enviarButton = findViewById(R.id.enviar);
 
@@ -36,12 +38,13 @@ public class anadirProductoActivity extends AppCompatActivity {
                 String categoria = categoriaEditText.getText().toString();
                 String descripcion = descripcionEditText.getText().toString();
                 String stockStr = stockEditText.getText().toString();
+                String imagen = imagenEditText.getText().toString();
 
-                if (nombre.isEmpty() || precio.isEmpty() || categoria.isEmpty() || descripcion.isEmpty() || stockStr.isEmpty()) {
+                if (nombre.isEmpty() || precio.isEmpty() || categoria.isEmpty() || descripcion.isEmpty() || stockStr.isEmpty() || imagen.isEmpty()) {
                     showToast("Por favor, completa todos los campos");
                 } else {
                     int stock = Integer.parseInt(stockStr);
-                    long newRowId = insertProducto(nombre, precio, categoria, descripcion, stock);
+                    long newRowId = insertProducto(nombre, precio, categoria, descripcion, stock, imagen);
                     if (newRowId != -1) {
                         showToast("Producto agregado correctamente");
                         clearFields();
@@ -53,7 +56,7 @@ public class anadirProductoActivity extends AppCompatActivity {
         });
     }
 
-    private long insertProducto(String nombre, String precio, String categoria, String descripcion, int stock) {
+    public long insertProducto(String nombre, String precio, String categoria, String descripcion, int stock, String imagen) {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("nombre", nombre);
@@ -61,6 +64,8 @@ public class anadirProductoActivity extends AppCompatActivity {
         values.put("categoria", categoria);
         values.put("descripcion", descripcion);
         values.put("stock", stock);
+        values.put("imagen", imagen);
+
         long newRowId = -1;
 
         try {
@@ -69,6 +74,10 @@ public class anadirProductoActivity extends AppCompatActivity {
             e.printStackTrace();
         } finally {
             db.close();
+        }
+        if (newRowId != -1) {
+            // Llama al método insertarProducto con la ruta de la imagen
+            dbHelper.insertarProducto(db, nombre, categoria, descripcion, Double.parseDouble(precio), stock, imagen);
         }
         return newRowId;
     }
@@ -83,5 +92,6 @@ public class anadirProductoActivity extends AppCompatActivity {
         categoriaEditText.setText("");
         descripcionEditText.setText("");
         stockEditText.setText("");
+        imagenEditText.setText("");
     }
 }
